5c7a3c4c903c3ed3225ae83c9bc7690eb205e5a4
Byron1716 <byron281728@gmail.com>
2025-06-28 08:04:50 -0500

    Práctica POO en C#

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string a = "Bienvenidos a la";
+string b = " Universidad Estatal Amzónica";
+string c = " UEA";
+
+Console.WriteLine("Hola a todos " + a + b + c);
+Console.WriteLine($"Hola a todos {a} {b} {c}");
